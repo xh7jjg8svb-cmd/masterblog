61205ecdb65c392014548cb03e5aca3d3abfe0a1
xh7jjg8svb-cmd <sullivan.janisch@icloud.com>
2026-02-21 13:12:51 +0100

    Added update route and created update function

--- app.py
+++ app.py
@@ -53,6 +53,28 @@ def delete(post_id):
     save_posts(posts)
     return redirect(url_for('index'))
 
+@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+def update(post_id):
+    posts = load_posts()
+    # Blogpost mit passender ID finden
+    post = next((p for p in posts if p["id"] == post_id), None)
+    if post is None:
+        return "Post not found", 404
+
+    if request.method == 'POST':
+        # Formulardaten abrufen
+        post['author'] = request.form.get('author')
+        post['title'] = request.form.get('title')
+        post['content'] = request.form.get('content')
+
+        # Posts speichern
+        save_posts(posts)
+
+        return redirect(url_for('index'))
+
+    # GET: Formular anzeigen
+    return render_template('update.html', post=post)
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
